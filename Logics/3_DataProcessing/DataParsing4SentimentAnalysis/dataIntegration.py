0239d61c3ca6c217766076df1080eae81780d27b
Federico Bianchi <federicobianchi14@gmail.com>
2023-07-19 17:29:16 +0200

    Community detection integration + analysis

--- Logics/3_DataProcessing/DataParsing4SentimentAnalysis/dataIntegration.py
+++ Logics/3_DataProcessing/DataParsing4SentimentAnalysis/dataIntegration.py
@@ -20,13 +20,8 @@ for csv in all_csv_bert[1:]:
     tmp = pd.read_csv(csv, error_bad_lines=False, dtype=str)
     df_bert = pd.concat([df_bert, tmp])
 
-print(len(df_spark))
-print(len(df_bert))
-
 df_spark = df_spark.rename(columns={'sentiment': 'sentiment_spark'}).drop(['author_id','conversation_author_id','conversation_id', 'timestamp', 'text'], axis=1)
 df_bert = df_bert.rename(columns={'sentiment': 'sentiment_bert'}).drop(['author_id','conversation_author_id','conversation_id', 'timestamp', 'parsed_text'], axis=1)
 df_bert['sentiment_bert'] = df_bert['sentiment_bert'].apply(string_to_array)
 df_tot = df_bert.merge(df_spark, how='inner', on='tweet_id')
-print(df_bert)
-print(df_tot)
 df_tot.to_csv('D:\Projects\BIG_DATA_TWITTER_ANALYSIS\Dataset\CuratedData\DataForSentimentAnalysis\\all_sentiment.csv', index=False)
